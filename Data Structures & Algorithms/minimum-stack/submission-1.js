329727076cac57f1da836d2b9b1cfd625a7a3be1
Tarun <107906206+tarun4164@users.noreply.github.com>
2026-09-14 09:27:51 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.js
@@ -0,0 +1,39 @@
+class MinStack {
+    constructor() {
+        this.stack = []
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val)
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        let min = Infinity;
+        for(let i=0; i < this.stack.length ; i++){
+            min = Math.min(min,this.stack[i])
+        }
+        return min
+
+    }
+}
